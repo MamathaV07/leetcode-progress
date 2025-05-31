a3183c3a88e543e2dd18813941e274b2eb18ba2e
Mamatha <mamathavadivel043@gmail.com>
2025-05-31 20:25:03 +0530

    Added palindrome number problem

---
+++ Easy/palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = str(x)
+        left=0
+        right=len(num)-1
+        while left<=right:
+            if num[left] == num[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+
+        return Truec
